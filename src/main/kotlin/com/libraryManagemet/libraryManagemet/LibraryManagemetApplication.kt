7960f8d81cfbff9a62e58c418c043676cf82985d
abedtamboli <abedtamboli@gmail.com>
2023-07-21 12:23:05 +0530

    initial setup

---
+++ src/main/kotlin/com/libraryManagemet/libraryManagemet/LibraryManagemetApplication.kt
@@ -0,0 +1,11 @@
+package com.libraryManagemet.libraryManagemet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryManagemetApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryManagemetApplication>(*args)
+}
---
+++ src/test/kotlin/com/libraryManagemet/libraryManagemet/LibraryManagemetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.libraryManagemet.libraryManagemet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryManagemetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
